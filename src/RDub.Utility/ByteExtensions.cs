4f69ee632852e183ce670ee824d4b5effb77da20
Ryan Wentzel <ryan.wentzel@gmail.com>
2012-11-02 09:23:12 -0700

    Initial commit.

---
+++ src/RDub.Utility/ByteExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace RDub.Utility
+{
+    /// <summary>
+    /// Extension methods that operate on bytes.
+    /// </summary>
+    public static class ByteExtensions
+    {
+        private const string HexFormatString = "x2";
+
+        /// <summary>
+        /// Converts a byte array to its hexadecimal string representation.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static string ToHexString(this byte[] buffer)
+        {
+            var builder = new StringBuilder();
+            foreach (var item in buffer)
+            {
+                builder.Append(item.ToString(HexFormatString));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/RDub.Utility/EnumerableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RDub.Utility
+{
+    /// <summary>
+    /// Extension methods that operate on enumerable collections.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Performs the specified action on each element of the <see cref="IEnumerable{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/></typeparam>
+        /// <param name="source">An <see cref="IEnumerable{T}"/> containing items on which to perform an action.</param>
+        /// <param name="action">The <see cref="Action{T}"/> delegate to perform on each element of the <see cref="IEnumerable{T}"/>.</param>
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null) throw new NullReferenceException();
+            if (action == null) throw new ArgumentNullException("action");
+
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
